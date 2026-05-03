552620b7d26ace9de93068996a4a7276bcd1e868
RayyanFurqan <140888668+RayyanFurqan@users.noreply.github.com>
2026-05-03 18:29:42 +0500

    Add files via upload

---
+++ include/HospitalException.h
@@ -0,0 +1,38 @@
+#pragma once
+
+class HospitalException {
+protected:
+    char message[200];
+public:
+    HospitalException(const char* msg);
+    virtual const char* what() const;
+    virtual ~HospitalException() {}
+};
+
+class FileNotFoundException : public HospitalException {
+public:
+    FileNotFoundException(const char* m = "Required file not found.")
+        : HospitalException(m) {
+    }
+};
+
+class InsufficientFundsException : public HospitalException {
+public:
+    InsufficientFundsException(const char* m = "Insufficient balance.")
+        : HospitalException(m) {
+    }
+};
+class InvalidInputException : public HospitalException {
+public:
+    InvalidInputException(const char* m = "Invalid input.")
+        : HospitalException(m) {
+    }
+};
+
+class SlotUnavailableException : public HospitalException {
+public:
+    SlotUnavailableException(const char* m = "Time slot already taken.")
+        : HospitalException(m) {
+    }
+};
+
